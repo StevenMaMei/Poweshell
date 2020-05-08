c84ee999c9539549a8d76ae400a9c674b67b863a
StevenMaMei <33093117+StevenMaMei@users.noreply.github.com>
2020-05-08 18:30:58 -0500

    project started

---
+++ proyecto/script.ps1
@@ -0,0 +1,19 @@
+$option = read-host "Introduzca la opcion: `n
+1. Desplegar los cinco procesos que mas CPU este consumiendo en ese momento.`n
+2. Desplegar los filesystems o discos conectados a la maquina. Incluir para cada disco su
+tamano y la cantidad de espacio libre (en bytes).`n
+3. Desplegar el nombre y el tamano del archivo mas grande almacenado en un disco o
+filesystem que el usuario debera especificar. El archivo debe aparecer con su trayectoria
+completa.`n
+4. Cantidad de memoria libre y cantidad del espacio de swap en uso (en bytes y porcentaje).`n
+5. Numero de conexiones de red activas actualmente (en estado ESTABLISHED).`n "
+
+if($option -eq 1){
+	Get-Process | Sort-Object cpu -Descending | Select-Object -first 5;
+}
+elseif($option -eq 2){
+	Get-WmiObject -Class Win32_LogicalDisk | Select-Object name,@{n='Tamaño (Bytes)'; e={$_.size}},@{n='Espacio libre (Bytes)'; e={$_.freespace}};
+}
+elseif($option -eq 3){}
+elseif($option -eq 4){}
+elseif($option -eq 5){}
